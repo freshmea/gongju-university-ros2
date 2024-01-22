952d3913734bc06b7c814b24592a7e7cd1760803
freshmea <freshmea@naver.com>
2024-01-22 16:26:35 +0900

    move turtlesim

--- gongju_ros/move_turtlesim.py
+++ gongju_ros/move_turtlesim.py
@@ -1,16 +1,25 @@
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
 
 class Move_turtle(Node):
     def __init__(self):
         super().__init__('move_turtle') #type: ignore
-        self.create_timer(1, self.print_callback)
-        self.n = 0
+        self.create_timer(0.1, self.pub_callback)
+        self.create_timer(1/30, self.update_callback)
+        self.pub = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+        self.msg = Twist()
         
-    def print_callback(self):
-        print('timer test', self.n)
-        self.n += 1
+    def pub_callback(self):
+        self.pub.publish(self.msg)
+
+    def update_callback(self):
+        # create your idea
+        self.msg.angular.z = 2.0
+        self.msg.linear.x += 0.01
+        if self.msg.linear.x > 10:
+            self.msg.linear.x = 0.0
 
 def main():
     rclpy.init()
