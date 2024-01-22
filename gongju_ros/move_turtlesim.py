93b5190a5c2a4d9e91a1d59a73a82e9d4298c412
freshmea <freshmea@naver.com>
2024-01-22 15:12:13 +0900

    first node

---
+++ gongju_ros/move_turtlesim.py
@@ -0,0 +1,5 @@
+def main():
+    print('this is gongju test')
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'move_turtlesim = gongju_ros.move_turtlesim:main'
         ],
     },
 )
