d93f8abe09ffc8649dc21dc8e43a574e06238802
freshmea <freshmea@naver.com>
2024-01-22 15:48:55 +0900

    timer test

--- gongju_ros/move_turtlesim.py
+++ gongju_ros/move_turtlesim.py
@@ -1,5 +1,21 @@
+import rclpy
+from rclpy.node import Node
+
+n = 0
+
+def print_callback():
+    global n
+    print('timer test', n)
+    n += 1
+
 def main():
-    print('this is gongju test')
+    rclpy.init()
+    node =Node('move_turtle') #type: ignore
+    node.create_timer(1, print_callback)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.destroy_node()
 
 if __name__ == '__main__':
     main()
