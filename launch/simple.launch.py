ed0a1b3adc76ddcb7eaa5dab1549b36793841da5
freshmea <freshmea@naver.com>
2024-01-23 14:39:42 +0900

    launch

--- launch/simple.launch.py
+++ launch/simple.launch.py
@@ -1,1 +1,29 @@
 # simple.launch.py
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package='gongju_ros',
+                executable='simple_pub',
+                output='screen'
+                ), 
+            Node(
+                package='gongju_ros',
+                executable='simple_sub',
+                output='screen'
+                ),
+            Node(
+                package='gongju_ros',
+                executable='simple_time_pub',
+                output='screen'
+                ),
+            Node(
+                package='gongju_ros',
+                executable='simple_time_sub',
+                output='screen'
+                )
+        ]
+        )
