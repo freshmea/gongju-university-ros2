f5866ed2ba932b62d344f8214f49e5cb029501bd
freshmea <freshmea@naver.com>
2024-01-23 16:15:59 +0900

    parameter yaml

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (share_dir +'/launch', glob(os.path.join('launch', '*.launch.py')))
+        (share_dir +'/launch', glob(os.path.join('launch', '*.launch.py'))),
+        (share_dir +'/param', glob(os.path.join('param', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
